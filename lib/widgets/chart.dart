b7bcf1a1f124f3e3fa32499e2a876a98aa97f784
Furkan Ayyıldız <furkan199905@gmail.com>
2022-11-28 22:36:15 +0300

    chart added

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+//pages
 import 'package:personal_spending_app/models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransaction;
@@ -30,16 +33,35 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + (item["amount"] as double);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print(groupedTransactionValues);
     return Card(
       elevation: 8,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
-          return Text(" ${data["day"]} : ${data["amount"]} ");
-        }).toList(),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                (data["day"] as String),
+                (data["amount"] as double),
+                totalSpending == 0.0
+                    ? 0.0
+                    : (data["amount"] as double) / totalSpending,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
