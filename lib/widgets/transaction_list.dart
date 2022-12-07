3a9f3e2d43f34f0542fdaa11ba1d2d0f181ae6d5
Furkan Ayyıldız <furkan199905@gmail.com>
2022-12-07 17:24:50 +0300

    Delete button style in lanscape mod and mediaquery managing

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -56,11 +56,20 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat.yMMMd().format(transactions[index].date),
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () => deleteTx(transactions[index].id),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 460
+                      ? TextButton.icon(
+                          onPressed: () => deleteTx(transactions[index].id),
+                          style: TextButton.styleFrom(
+                            primary: Colors.red,
+                          ),
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          label: Text("Delete"),
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => deleteTx(transactions[index].id),
+                        ),
                 ),
               );
             },
